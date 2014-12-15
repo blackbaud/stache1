305d6fd32a0ad7d9fdc9fa2c12fa0d4a11939190
Bobby <bobby.earl@blackbaud.com>
2014-12-15 14:28:01 -0500

    Playing around with Omnibar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
-!function($) {
+;(function($, window, document, undefined) {
+  
   BBAUTH.Omnibar.load(document.getElementById("omnibar"), {
     enableSearch: true,
     serviceName: 'Developer',
+    signInRedirectUrl: document.location.href,
+    signOutRedirectUrl: '',
+    afterLoad: function() {
+      console.log('Omnibar Loaded');
+    },
     userLoaded: function(user) {
       console.log(user);
     }
   });
-}(jQuery);
+  
+}(jQuery, window, document);
