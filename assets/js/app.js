28b9dcee9418644bc23b5e188e7bf92b35207f73
Bobby Earl <bobby.earl@blackbaud.com>
2015-01-12 08:52:37 -0500

    Changed App Name

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@
   // Initialize the Omnibar
   BBAUTH.Omnibar.load(document.getElementById("omnibar"), {
     enableSearch: false, // FYI, toggle the "searching" class on "searchContainer" to get the spinner
-    serviceName: 'Developer',
+    serviceName: 'RENXT Developer',
     signInRedirectUrl: document.location.href,
     signOutRedirectUrl: '',
     afterLoad: function() {
