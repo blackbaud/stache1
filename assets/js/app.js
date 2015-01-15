66b3759ca4b497ba0867285d4c0c9b6f61c55fdd
Bobby <bobby.earl@blackbaud.com>
2015-01-15 15:08:27 -0500

    Exploring options

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
   // Initialize the Omnibar
   BBAUTH.Omnibar.load(document.getElementById("omnibar"), {
     enableSearch: false, // FYI, toggle the "searching" class on "searchContainer" to get the spinner
+    enableHelp: false,
     serviceName: 'RENXT Developer',
     signInRedirectUrl: document.location.href,
     signOutRedirectUrl: ''
