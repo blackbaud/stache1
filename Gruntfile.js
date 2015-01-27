93e74b2262f5ec66b975ddd475f18597be13b6c3
Bobby Earl <bobby.earl@blackbaud.com>
2015-01-27 12:53:47 -0500

    Implemented grunt nuget workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,22 @@
+/**
+* Grunt configuration file.
+* Bobby Earl, 2015-01-27
+*
+* NOTES
+*   There is a current bug where 'local echo' can't be disabled when running the skyui-tfs-clone task.
+*   This means a user's password will be visible.  More investigation into the problem is necessary.
+**/
 module.exports = function(grunt) {
+  
+  // Load the required node modules
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-http');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-zip');
 
+  // Initialize our configuration
   grunt.initConfig({
     
     // Reads package information
@@ -8,45 +25,101 @@ module.exports = function(grunt) {
     // Paths used through this gruntfile
     paths: {
       tfs:  'https://tfs.blackbaud.com/tfs/DefaultCollection/',
-      skyRemote: '$/Products/REx/Styles/Sky/DEV/Sky/Content/Styles/Sky/',
-      skyLocal: '_sass/Sky/',
+      skyTfsRemote: '$/Products/REx/Styles/Sky/DEV/Sky/Content/Styles/Sky/',
+      skyTfsLocal: '_sass/Sky/',
+      skyJsonRemote: [
+        'http://tfs-sym.blackbaud.com:81/nuget/FindPackagesById()',
+        '?id=\'Blackbaud.SkyUI.Mixins\'',
+        '&$orderby=Published desc',
+        '&$top=1'].join(''),
+      skyJsonLocal: '_nuget/Blackbaud.SkyUI.Mixins.json',
+      skyZipRemote: '', // Set by http:skyui-json
+      skyZipLocal: '_nuget/Blackbaud.SkyUI.Mixins.zip',
+      skyZipExpanded: '_nuget/Blackbaud.SkyUI.Mixins/',
       renxtConfig: '_config.yml,_config.renxt.yml',
       fenxtConfig: '_config.yml,_config.fenxt.yml'
     },
     
+    http: {
+      'skyui-nuget-json': {
+        options: {
+          url: '<%= paths.skyJsonRemote %>',
+          json: true,
+          callback: function(error, response, body) {
+            try {
+              grunt.config.set('paths.skyZipRemote', body.d[0].__metadata.media_src);
+              console.log('Found latest SkyUI nuget: %s', grunt.config('paths.skyZipRemote'));
+            } catch(err) {
+              console.log('Error parsing NuGet response.');
+              console.log(err);
+            }
+          }
+        },
+        dest: '<%= paths.skyJsonLocal %>'
+      }
+    },
+    
+    // Expose our CURL / SkyUI NuGet tasks
+    curl: {
+      'skyui-nuget-download': {
+        src: '<%= paths.skyZipRemote %>',
+        dest: '<%= paths.skyZipLocal %>'
+      }
+    },
+    
+    // Expose our zip tasks
+    unzip: {
+      'skyui-nuget-unzip': {
+        src: '<%= paths.skyZipLocal %>',
+        dest: '<%= paths.skyZipExpanded %>'
+      }
+    },
+    
+    // Expose our copy tasks
+    copy: {
+      'skyui-nuget-copy': {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.skyZipExpanded %>/Content/Content/Styles/Sky/',
+          src: ['**'],
+          dest: '<%= paths.skyTfsLocal %>'
+        }]
+      }
+    },
+    
     // Expose our TFS / SkyUI tasks
     shell: {
-      skyuiClone: {
-        command: 'git tf clone <%= paths.tfs %> <%= paths.skyRemote %> <%= paths.skyLocal %>'
+      'skyui-tfs-clone': {
+        command: 'git tf clone <%= paths.tfs %> <%= paths.skyTfsRemote %> <%= paths.skyTfsLocal %>'
       },
-      skyuiFetch: {
+      'skyui-tfs-fetch': {
         command: 'git fetch <%= paths.skyLocal %>'
       }
     },
     
     // Expose jekyll tasks
     jekyll: {
-      renxtServe: {
+      'renxt-serve': {
         options: {
           serve: true,
           baseurl: '""',
           config: '<%= paths.renxtConfig %>'
         }
       },
-      renxtBuild: {
+      'renxt-build': {
         options: {
           serve: false,
           config: '<%= paths.renxtConfig %>'
         }
       },
-      fenxtServe: {
+      'fenxt-serve': {
         options: {
           serve: true,
           baseurl: '""',
           config: '<%= paths.fenxtConfig %>'
         }
       },
-      fenxtBuild: {
+      'fenxt-build': {
         options: {
           serve: false,
           config: '<%= paths.fenxtConfig %>'
@@ -55,8 +128,17 @@ module.exports = function(grunt) {
     }
   });
   
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-jekyll');
+  // Possibly not necessary in this context, but I'm namespacing all our commands.
+  // It's obviously still possible to call the original grunt commands.
+  grunt.registerTask('blackbaud:skyui-tfs-clone', 'shell:skyui-tfs-clone');
+  grunt.registerTask('blackbaud:skyui-tfs-fetch', 'shell:skyui-tfs-fetch');
+  grunt.registerTask('blackbaud:skyui-nuget', [
+    'http:skyui-nuget-json', 
+    'curl:skyui-nuget-download', 
+    'unzip:skyui-nuget-unzip',
+    'copy:skyui-nuget-copy'
+  ]);
   
+  // Not sure what the "default" task should do yet
   //grunt.registerTask('default', ['copy', 'concat', 'sass', 'watch']);
 };
--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 RENXT Developer
 =============================
 
-Follow the steps to setup jekyll from https://help.github.com/articles/using-jekyll-with-pages#installing-jekyll.
-
-The config file structure is slightly more complicated than a normal jekyll project - the reason being is we're attempting to support three build environments.  One of the major differences between these three environments is the <code>baseurl</code> necessary to function properly.
-
-### Building for GitHub Pages
-
-There is no work to be done for this environment.  Their build automatically looks at the _config.yml file.
 
 ### Building for Development Environment
 
@@ -16,23 +9,21 @@ There is no work to be done for this environment.  Their build automatically loo
 - NPM
 - git
 - <a href="http://gittf.codeplex.com">git-tf</a>
+- Jekyll
  
 ** Please note these in turn have their own requirements for installation.
 
-<code>jekyll serve --baseurl ''</code>
-
 You will now be able to visit <a href="http://localhost:4000">http://localhost:4000</a> in your browser to view the site.  You should also notice that the site is being stored and served from the _site directory.  This directory is set to be ignored.
 
-### Building for the Production Environment
+### Available Grunt Commands
 
-<code>jekyll build --baseurl 'renxt'</code>
+- grunt shell:skyui-clone
+- grunt shell:skyui-fetch
+- grunt jekyll:renxt-serve
+- grunt jekyll:renxt-build
+- grunt jekyll:fenxt-serve
+- grunt jekyll:fenxt-build
 
 ### Contributing 
 
 If you would like to contribute to this code sample, please carefully read the [contributing documentation](https://github.com/blackbaud-community/Blackbaud-CRM/blob/master/CONTRIBUTING.md), which details the necessary workflow.  Included in those requirements is [signing the Contributor License Agreement](http://developer.blackbaud.com/cla).
-
-### Oh Hey, BTW
-
-If you're a passionate and knowledgable developer who enjoys working on challenging projects and equally cares about giving back to non-profits, consider joining the Blackbaud Team.  
-
-https://www.blackbaud.com/careers
