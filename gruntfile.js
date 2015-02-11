4249d8d9faaad64e3d08085e4397e6c351fd0e5a
Bobby <bobby.earl@blackbaud.com>
2015-02-11 10:54:24 -0500

    JSLint passing.

--- gruntfile.js
+++ gruntfile.js
@@ -11,33 +11,35 @@
 *   This means a user's password will be visible.  More investigation into the problem is necessary.
 **/
 
+/*jslint node: true, nomen: true, plusplus: true */
 'use strict';
-module.exports = function(grunt) {
+
+module.exports = function (grunt) {
   
   // Blackbaud Namespace
   var NS = 'blackbaud:',
-      tasks = [
-        'assemble',
-        'grunt-asciify',
-        'grunt-contrib-clean',
-        'grunt-contrib-concat',
-        'grunt-contrib-connect',
-        'grunt-contrib-cssmin',
-        'grunt-contrib-sass',
-        'grunt-contrib-uglify',
-        'grunt-contrib-watch',
-        'grunt-filerev',
-        'grunt-newer',
-        'grunt-nugetter',
-        'grunt-shell',
-        'grunt-usemin'
-      ];
+    tasks = [
+      'assemble',
+      'grunt-asciify',
+      'grunt-contrib-clean',
+      'grunt-contrib-concat',
+      'grunt-contrib-connect',
+      'grunt-contrib-cssmin',
+      'grunt-contrib-sass',
+      'grunt-contrib-uglify',
+      'grunt-contrib-watch',
+      'grunt-filerev',
+      'grunt-newer',
+      'grunt-nugetter',
+      'grunt-shell',
+      'grunt-usemin'
+    ];
   
   // Load the required node modules
   tasks.forEach(grunt.loadNpmTasks);
   
   // Disable grunt headers
-  grunt.log.header = function() {} 
+  grunt.log.header = function () {};
 
   // Initialize our configuration
   // For my sanity, please alphabetize any tasks after pkg and site.
@@ -70,15 +72,15 @@ module.exports = function(grunt) {
           font: 'cybermedium',
           log: true
         }
-      },
+      }
     },
     
     // Static site gen
     assemble: {
       options: {
-        assets: '<%= site.app_build %>assets/',        
+        assets: '<%= site.app_build %>assets/',
         data: '<%= site.app_data %>*.*',
-		helpers: ['<%= site.app_helpers %>**/*.js'],
+        helpers: ['<%= site.app_helpers %>**/*.js'],
         //partials: ['<%= site.app_partials %>**/*.*'],
         layoutdir: '<%= site.app_layouts %>',
         layout: 'base.hbs',
@@ -113,7 +115,7 @@ module.exports = function(grunt) {
             '<%= site.app_src %>'
           ],
           livereload: true,
-          port: 4000 
+          port: 4000
         }
       }
     },
@@ -143,11 +145,11 @@ module.exports = function(grunt) {
           // Instead of being able to use dest, I'm having to remove '{ value: [...]}'
           // This caused errors in assemble.
           json: true,
-          callback: function(error, response, body) {
+          callback: function (error, response, body) {
             if (error) {
               grunt.log.writeln(error);
             } else {
-              grunt.file.write('<%= site.app_data %>operations.json', JSON.stringify(body.value)); 
+              grunt.file.write('<%= site.app_data %>operations.json', JSON.stringify(body.value));
             }
           }
         }
@@ -228,16 +230,20 @@ module.exports = function(grunt) {
   });
   
   // Current showing help message as default task
-  grunt.registerTask('welcome', function() {
+  grunt.registerTask('welcome', function () {
 
-    var msg = 'Listed below are available grunt commands in version %s:';
+    var msg = 'Listed below are available grunt commands in version %s:',
+      tasks = [],
+      task,
+      i,
+      j;
+    
     grunt.log.writeln(msg.green.bold, grunt.config('pkg.version'));
     grunt.log.writeln('');
     
     // Filter BB tasks.  Saving to array to sort them by name.
-    var tasks = [];
-    for ( var task in grunt.task._tasks) {
-      if (task.indexOf(NS) == 0) {
+    for (task in grunt.task._tasks) {
+      if (grunt.task._tasks.hasOwnProperty(task) && task.indexOf(NS) === 0) {
         tasks.push(task);
       }
     }
@@ -246,7 +252,8 @@ module.exports = function(grunt) {
     //tasks.sort();
     
     // Display our tasks
-    for (var i = 0, j = tasks.length; i < j; i++) {
+    j = tasks.length;
+    for (i; i < j; i++) {
       grunt.log.writeln(tasks[i].bold);
       grunt.log.writeln(grunt.task._tasks[tasks[i]].info);
       grunt.log.writeln('');
@@ -254,19 +261,19 @@ module.exports = function(grunt) {
   });
   
   grunt.registerTask(
-    NS + 'serve', 
-    'Serve the documentation', 
+    NS + 'serve',
+    'Serve the documentation',
     [
       'clean',
       'newer:assemble',
-      'connect', 
+      'connect',
       'watch'
     ]
   );
   
   grunt.registerTask(
-    NS + 'build', 
-    'Build the documentation', 
+    NS + 'build',
+    'Build the documentation',
     [
       'clean',
       'assemble',
@@ -286,25 +293,25 @@ module.exports = function(grunt) {
       'shell:npm-install',
       'shell:bower-install',
       NS + 'skyui-nuget',
-      NS + 'portal-get-operations',
+      NS + 'portal-get-operations'
     ]
   );
 
   grunt.registerTask(
-    NS + 'portal-get-operations', 
-    'Downloads the list of operations for the default (or specified) API.', 
+    NS + 'portal-get-operations',
+    'Downloads the list of operations for the default (or specified) API.',
     'http:portal-get-operations'
   );
   
   grunt.registerTask(
-    NS + 'skyui-tfs-clone', 
-    'Clones the latest version of SkyUI from TFS', 
+    NS + 'skyui-tfs-clone',
+    'Clones the latest version of SkyUI from TFS',
     'shell:skyui-tfs-clone'
   );
   
   grunt.registerTask(
-    NS + 'skyui-tfs-fetch', 
-    'Fetches the latest version of SkyUI from TFS', 
+    NS + 'skyui-tfs-fetch',
+    'Fetches the latest version of SkyUI from TFS',
     'shell:skyui-tfs-fetch'
   );
   
@@ -316,9 +323,9 @@ module.exports = function(grunt) {
   
   // Display help message as default task
   grunt.registerTask('default', [
-    'asciify:one', 
-    'asciify:two', 
-    'asciify:three', 
+    'asciify:one',
+    'asciify:two',
+    'asciify:three',
     'welcome'
   ]);
   
