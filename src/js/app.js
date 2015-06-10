43797235da143ac44d5b178017bae29cbd80454e
Bobby Earl <bobby.earl@blackbaud.com>
2015-06-10 09:15:53 -0400

    Sidebar styling

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,17 @@
 /*global jQuery */
 (function ($, window, document, undefined) {
   'use strict';
-  
+
   var body = $('body'),
     sidebar = $('.sidebar'),
     sidebarNav = $('.nav-sidebar'),
     sidebarHeading = sidebar.find('li.heading'),
     height = 0;
-  
+
   // Scrollspy + affix only run if there are li.heading's on the page
-  if (sidebarHeading.length) {
-    
+  // And the sidebar isn't taller than the page
+  if (sidebarHeading.length && $(window).height() > sidebar.height()) {
+
     // Affix
     sidebarNav.affix({
       offset: {
@@ -29,27 +30,27 @@
     body.scrollspy({
       target: '.headings'
     });
-    
+
   }
-  
+
   // Parallax background
   $.stellar({
     horizontalScrolling: false
   });
-  
+
   // Equal Height
   $('.equal-height').each(function () {
     var h = $(this).outerHeight();
     height = h > height ? h : height;
   }).css('min-height', height + 'px');
-  
+
   // Show on Hover
   $('.has-hover').each(function () {
     $(this).hover(function () {
       $(this).toggleClass('is-hover');
     });
   });
-  
+
   // Smooth scroll
   $('a.smooth-scroll').click(function (e) {
     e.preventDefault();
@@ -57,29 +58,29 @@
     // Manually making #top = 0
     var href = $(this).attr('href'),
       top = href === '#top' ? 0 : $($(this).attr('href')).offset().top;
-    
+
     $('html, body').animate({
       scrollTop: top
     }, 1000);
   });
-  
+
   // Tooltips
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
-  
+
   // Searching
   var q = getParameterByName('q');
   if (q !== '') {
     $('#q').val(q);
   }
-  
-  
+
+
   function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
   }
-  
+
 }(jQuery, window, document));
