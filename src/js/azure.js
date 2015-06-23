d08c215a39a3a31a261d7fd09b569ed414ee7f4c
Bobby Earl <bobby.earl@blackbaud.com>
2015-06-23 17:46:23 -0400

    Fixing JS typo

--- src/js/azure.js
+++ src/js/azure.js
@@ -2,7 +2,7 @@
   $(function() {
 
     $('head').append('<link rel="icon" href="//blackbaud-community.github.io/developer.blackbaud.com-renxt/assets/img/favicon.ico" type="image/ico">');
-    $('.zone-navigation a[href="/docs/services"]').closest('li').toggleClass('active', !~location.href.indexOf('/docs/services/'));
+    $('.zone-navigation a[href="/docs/services"]').closest('li').toggleClass('active', !~location.href.indexOf('/docs/services'));
 
   });
 })(jQuery, window, document);
