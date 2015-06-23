eb0f766fa963d3dc1cd67d02fe7ed79d198d6d08
Bobby Earl <bobby.earl@blackbaud.com>
2015-06-23 17:16:34 -0400

    Additioanl Azure Tweaks

--- src/js/azure.js
+++ src/js/azure.js
@@ -1,16 +1,8 @@
----
-layout: blank
----
-
 ;(function($, window, document, undefined) {
-  
-  {% include_relative _shared-omnibar.js isAzure=true %}
-  
   $(function() {
-    $('body').addClass('bb-omnibar-height-padding'); 
-    $('#navigation .navbar-nav > li:eq(4)').addClass('active');
-    $('.zone-aside-first .navbar-nav').insertBefore($('.zone-aside-first .nav-pills'));
+
     $('head').append('<link rel="icon" href="//blackbaud-community.github.io/developer.blackbaud.com-renxt/assets/img/favicon.ico" type="image/ico">');
+    $('.zone-navigation a[href="/docs/services"]').closest('li').toggleClass('active', ~location.href.indexOf('/docs/services/'));
+
   });
-  
 })(jQuery, window, document);
