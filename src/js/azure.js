0db3e6c9fce6a4c30b3f9677c89faa1ea8f48b31
Bobby Earl <bobby.earl@blackbaud.com>
2015-06-25 08:32:20 -0400

    Azure Styling Updates

--- src/js/azure.js
+++ src/js/azure.js
@@ -3,6 +3,7 @@
 
     $('head').append('<link rel="icon" href="//blackbaud-community.github.io/developer.blackbaud.com-renxt/assets/img/favicon.ico" type="image/ico">');
     $('.zone-navigation a[href="/docs/services"]').closest('li').toggleClass('active', location.href.indexOf('/docs/services') > -1);
+    $('.zone-aside-first').toggleClass('col-md-2 col-md-3');
 
   });
 })(jQuery, window, document);
