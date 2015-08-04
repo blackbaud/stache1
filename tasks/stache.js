2e66cf6d340c5209121340ef8dfd97242defaa4c
Bobby Earl <bobby.earl@blackbaud.com>
2015-08-04 09:15:27 -0400

    Patch to include fonts

--- tasks/stache.js
+++ tasks/stache.js
@@ -199,9 +199,6 @@ module.exports = function (grunt) {
 
     cssmin: {
       target: {
-        options: {
-            processImport: false
-        },
         files: [{
           expand: true,
           cwd: '<%= stache.config.build %>',
