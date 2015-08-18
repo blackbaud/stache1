bb19e75f1d677be52cee79e20d977836e9ca6ac8
Brandon Hare <brandon.hare@blackbaud.com>
2015-08-18 11:00:48 -0400

    added header to video description and made description automatically appear for the first video in the playlist

--- src/js/video.js
+++ src/js/video.js
@@ -24,12 +24,16 @@ $(document).ready(function () {
             scrollLeft: "+=320"
         }, 750);
     });
+  
+    $('#video-description-header').text($('.video-item').first().children('.video-title').text());
+    $('#video-description').text($(".video-item").first().data('description'));
 
     $(".video-item").on("click", function(event){
       event.preventDefault();
       var source = $(this).attr("href");
       source += "?autoplay=1&rel=0&showinfo=0&autohide=1";
       $("#video-iframe").attr("src", source);
+      $('#video-description-header').text($(this).children('.video-title').text());
       $('#video-description').text($(this).data('description'));
     })
 
